ce5acbab8300f218c5937921e1ef079443df69cb
autosam <autosam.sm@gmail.com>
2024-12-25 22:45:45 +0330

    add missing suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { ProjectsContainer } from "@/components/ProjectsContainer";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <div className="text-xs">
       {/* <div className="mt-96" /> */}
-      <ProjectsContainer />
+      <Suspense>
+        <ProjectsContainer />
+      </Suspense>
     </div>
   );
 }
