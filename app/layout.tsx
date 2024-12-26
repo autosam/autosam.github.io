06901a2d02a25e3265272355b6e43bdba3b9e1bc
autosam <autosam.sm@gmail.com>
2024-12-26 18:01:07 +0330

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { Breadcrumb } from "@/components/Breadcrumb";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const font = Tomorrow({ weight: "400", subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
           />
           <div className="p-4">{children}</div>
           <Footer />
+          <GoogleAnalytics gaId="G-1THW58E0S2" />
         </div>
       </body>
     </html>
