63fd5270e29893c1bb75d60cc2a73a558ba980a7
saman.mohammadi <autosam.sm@gmail.com>
2024-12-28 10:14:11 +0330

    fix hero text not expanding on high rez

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,8 +3,8 @@ import { uid } from "uid";
 export const Hero = () => {
   return (
     <div className="w-full h-72 bg-black text-white overflow-hidden relative border border-black">
-      <label className=" absolute z-10 top-0 left-0 w-full h-full text-9xl flex gap-2 -m-8 animate-bounce">
-        {new Array(3).fill(1).map(() => (
+      <label className=" absolute z-10 top-0 left-0 w-full h-full text-9xl flex gap-2 -m-8 justify-center animate-bounce">
+        {new Array(6).fill(1).map(() => (
           <div key={uid()} className="flex flex-col [&>*]:-my-4">
             <h1>SAMAN</h1>
             <h1>SAMAN</h1>
