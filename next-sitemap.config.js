41bedb418d4bf21a53a54ba5bf58276aeee277f2
autosam <autosam.sm@gmail.com>
2025-02-22 19:26:50 +0330

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL || "http://autosam.github.io/", // Your website's URL
+  generateRobotsTxt: true, // (Optional) Generates a robots.txt file
+  sitemapSize: 7000, // Number of URLs per sitemap file
+};
