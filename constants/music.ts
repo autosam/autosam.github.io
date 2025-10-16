a1f9ee3215544005c223892818688448ce5a7689
autosam <autosam.sm@gmail.com>
2025-10-16 23:20:24 +0330

    feat: update music

--- constants/music.ts
+++ constants/music.ts
@@ -1,5 +1,5 @@
 export const MUSIC = {
-  artist: "jixwang, VISION SOUND",
-  title: "Dancing Through Fantasies",
-  href: "https://open.spotify.com/album/0da6BSYSm4CwKozvjRw7c2",
+  artist: "Cosmograph",
+  title: "Jump Up",
+  href: "https://www.youtube.com/watch?v=HqqVjW8V9ms",
 };
